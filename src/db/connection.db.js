e761aa5f6e3177104fe30ae64a8e8dddf8458c6d
1802042 <rony16@cse.pstu.ac.bd>
2024-09-30 18:49:03 +0600

    Add cookie-parser and cors dependencies

--- src/db/connection.db.js
+++ src/db/connection.db.js
@@ -5,11 +5,10 @@ import {DB_NAME} from "../constants.js"
 const connectDB = async function (){
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-
-        console.log(`MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`)
+        console.log(`MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MongoDB connection error : " + error)
-        throw error 
+        console.log("MongoDB connection error : " + error);
+        throw error; 
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/connection.db.js"
+import { app } from "./app.js";
 
 dotenv.config({
     path: "../.env"
 })
 
-connectDB();
+connectDB()
+.then(() =>{
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`App is listening at port : ${port}`);
+    })
+})
+.catch((error) => {
+    console.log(`Database connection error : ${error}`)
+});
 
